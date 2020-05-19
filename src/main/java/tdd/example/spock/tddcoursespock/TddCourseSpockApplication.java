fc03114d19dded495ec822356a41304ab3d60c72
Keith Crowe <Kcrowe123@yahoo.com>
2020-05-19 14:31:54 -0500

    Initial commit with spock dependencies

---
+++ src/main/java/tdd/example/spock/tddcoursespock/TddCourseSpockApplication.java
@@ -0,0 +1,13 @@
+package tdd.example.spock.tddcoursespock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddCourseSpockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddCourseSpockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tdd/example/spock/tddcoursespock/TddCourseSpockApplicationTests.java
@@ -0,0 +1,16 @@
+package tdd.example.spock.tddcoursespock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TddCourseSpockApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
